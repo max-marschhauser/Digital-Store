4c4f07cf9fc72def0a0ddc32766eed5fd040de09
max-marschhauser <max.marschhauser@gmail.com>
2022-09-09 17:49:31 +0200

    added sort function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
 import {} from "./displayAlbums.js"; // display albums from albumList.js onto the site
 import {} from "./cart.js"; // placing items into cart, removing items from cart, managing price, quantity and purchase button
 import {} from "./actionButtons.js"; // action buttons for site (filter, search, sort)
+import {} from "./search.js"; // defines function for search action button
