713c8fb7096047d801a58349cd9f15d8bd5381b4
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-09 12:38:35 +0200

    added more items in store

--- src/scripts/albumList.js
+++ src/scripts/albumList.js
@@ -2,11 +2,46 @@
 
 export let albumList = [
     {
-        title: "L.A.Woman",
-        author: "The Doors",
+        title: "Master of Reality",
+        author: "Black Sabbath",
         year: 1971,
-        price: 11.99,
-        imgSrc: "img/albums/The_Doors/LA_Woman.jpg",
+        price: 10.99,
+        imgSrc: "img/albums/Black_Sabbath/Master_of_Reality.jpg",
+    },
+    {
+        title: "Paranoid",
+        author: "Black Sabbath",
+        year: 1970,
+        price: 25.99,
+        imgSrc: "img/albums/Black_Sabbath/Paranoid.jpg",
+    },
+    {
+        title: "Vol. 4",
+        author: "Black Sabbath",
+        year: 1972,
+        price: 15.59,
+        imgSrc: "img/albums/Black_Sabbath/Vol_4.jpg",
+    },
+    {
+        title: "In Rock",
+        author: "Deep Purple",
+        year: 1970,
+        price: 17.29,
+        imgSrc: "img/albums/Deep_Purple/In_Rock.jpg",
+    },
+    {
+        title: "Mothership",
+        author: "Led Zeppelin",
+        year: 2007,
+        price: 13.99,
+        imgSrc: "img/albums/Led_Zeppelin/Mothership.jpg",
+    },
+    {
+        title: "Animals",
+        author: "Pink Floyd",
+        year: 1977,
+        price: 9.99,
+        imgSrc: "img/albums/Pink_Floyd/Animals.jpg",
     },
     {
         title: "Dark Side of the Moon",
@@ -16,11 +51,32 @@ export let albumList = [
         imgSrc: "img/albums/Pink_Floyd/Dark_Side_of_the_Moon.jpg",
     },
     {
-        title: "Paranoid",
-        author: "Black Sabbath",
+        title: "Wish You Were Here",
+        author: "Pink Floyd",
+        year: 1975,
+        price: 11.99,
+        imgSrc: "img/albums/Pink_Floyd/Wish_You_Were_Here.jpg",
+    },
+    {
+        title: "L.A.Woman",
+        author: "The Doors",
+        year: 1971,
+        price: 11.99,
+        imgSrc: "img/albums/The_Doors/LA_Woman.jpg",
+    },
+    {
+        title: "Morrison Hotel",
+        author: "The Doors",
         year: 1970,
-        price: 17.99,
-        imgSrc: "img/albums/Black_Sabbath/Paranoid.jpg",
+        price: 10.99,
+        imgSrc: "img/albums/The_Doors/Morrison_Hotel.jpg",
+    },
+    {
+        title: "The Doors",
+        author: "The Doors",
+        year: 1967,
+        price: 18.99,
+        imgSrc: "img/albums/The_Doors/The_Doors.jpg",
     },
     {
         title: "Waiting for the Sun",
@@ -29,11 +85,5 @@ export let albumList = [
         price: 9.99,
         imgSrc: "img/albums/The_Doors/Waiting_for_the_Sun.jpg",
     },
-    {
-        title: "Mothership",
-        author: "Led Zeppelin",
-        year: 2007,
-        price: 13.99,
-        imgSrc: "img/albums/Led_Zeppelin/Mothership.jpg",
-    },
+    
 ];
