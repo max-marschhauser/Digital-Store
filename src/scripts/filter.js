9f8ae1b91b796612712b3ac5c114776138b21da5
max-marschhauser <max.marschhauser@gmail.com>
2022-09-09 16:52:34 +0200

    made action buttons for site
    Filter, Search and Sort

--- src/scripts/filter.js
+++
@@ -1,9 +0,0 @@
-// filter which albums do you want to be displayed on site
-
-let filterButton = document.querySelector("[data-filter]");
-filterButton.addEventListener("click", filterAlbums);
-
-function filterAlbums(){
-let x = document.querySelectorAll("[data-albums-item]");
-x.forEach((item) => console.log(item));
-}
