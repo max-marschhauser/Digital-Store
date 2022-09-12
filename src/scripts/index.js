46fd65ba29c95e4d250a39758a67c1d81293589a
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-12 11:37:14 +0200

    changes to footer of the page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,9 @@ import {} from "./displayAlbums.js"; // display albums from albumList.js onto th
 import {} from "./cart.js"; // placing items into cart, removing items from cart, managing price, quantity and purchase button
 import {} from "./actionButtons.js"; // action buttons for site (filter, search, sort)
 import {} from "./search.js"; // defines function for search action button
+
+//COPYRIGHT DATE
+
+const copyrightDate = document.querySelector("[data-copyright-date]");
+
+copyrightDate.innerHTML = "&copy;" + new Date().getFullYear() + ".";
